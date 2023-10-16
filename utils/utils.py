782259418056ccbef3865a40fa34ee22a86b50bc
munishka-vijay <munishkav@gmail.com>
2023-10-16 17:04:29 +0530

    refactoring: added expiration and validation for jwt toekn

--- utils/utils.py
+++ utils/utils.py
@@ -16,14 +16,14 @@ def create_hashed_password(password):
 def generate_jwt_token(user_id, username):
 
     # Set expiry for jwt token
-    expiry=datetime.utcnow() + timedelta(hours=Config.JWT_ACCESS_TOKEN_EXPIRES)
 
-    # expiration = datetime.utcnow() + timedelta(hours=Config.JWT_ACCESS_TOKEN_EXPIRES)
-
+    expiration = datetime.utcnow() + timedelta(hours=Config.JWT_ACCESS_TOKEN_EXPIRES)
+    expiration_timestamp = expiration.timestamp()
     #Create payload using useri, username and expiry
     payload = {
         'user_id': user_id,
         'username': username,
+        'expiry':expiration_timestamp
     }
 
     # Generate the JWT token using the secret key from the Flask app configuration
@@ -31,6 +31,21 @@ def generate_jwt_token(user_id, username):
 
     return token
 
+# To check if user has entered correct password
 def check_password(password, hashedPassword):
     print(password, hashedPassword)
     return check_password_hash(hashedPassword, password)
+
+# To check if JWT Token is valid
+def check_jwt_token(token):
+    payload = jwt.decode(token, Config.JWT_SECRET_KEY, algorithms=['HS256'])
+    expiry_timestamp = payload['expiry']
+    expiry_datetime = datetime.fromtimestamp(expiry_timestamp)
+
+    # Now, you can check if the token has expired
+    if expiry_datetime < datetime.utcnow():
+        # Token has expired
+        return False
+
+    # Token is valid
+    return True
